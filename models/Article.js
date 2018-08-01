43d0cfa55674ebf7b323ce384f2376816dc6dc98
stephen <stephenrs7@gmail.com>
2018-08-01 13:06:17 -0700

    updates

--- models/Article.js
+++ models/Article.js
@@ -16,6 +16,14 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
+  date: {
+    type: String,
+    required: true
+  },
+  time: {
+    type: String,
+    required: true
+  },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
--- server.js
+++ server.js
@@ -49,7 +49,7 @@ if(process.env.MONGODB_URI) {
       var hbsObject = {
         article: data
       };
-      console.log(hbsObject);
+      // console.log(hbsObject);
       res.render("home", hbsObject);
     });
   });
@@ -70,29 +70,36 @@ if(process.env.MONGODB_URI) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(html);
       // Now, we grab every h2 within an article tag, and do the following:
-      $("article").each(function(i, element) {
+      $("article.story").each(function(i, element) {
   
         // Save an empty result object
         var result = {};
+        console.log("__________________________________________________________")
+        // console.log($(this));
   
         // Add the title and summary of every link, and save them as properties of the result object
         result.title = $(this).children("h2").text();
-        result.summary = $(this).children(".summary").text();
+        result.byline = $(this).children("p.byline").text();
+        result.date = $(this).children(".byline").children("time.timestamp").attr("datetime");
+        result.time = $(this).children("p.byline").children("time").data("eastern-timestamp");
         result.link = $(this).children("h2").children("a").attr("href");
-  
+        
+        // console.log($(this).children("p .byline"));
+        console.log($(this).children("h2").text());
         // Using our Article model, create a new entry
         // This effectively passes the result object to the entry (and the title and link)
+        // console.log(result);
         var entry = new db.Article(result);
         
         // Now, save that entry to the db
         entry.save(function(err, doc) {
           // Log any errors
           if (err) {
-            console.log(err);
+            // console.log(err);
           }
           // Or log the doc
           else {
-            console.log(doc);
+            // console.log(doc);
           }
         });
   
@@ -109,7 +116,7 @@ if(process.env.MONGODB_URI) {
     db.Article.find({}, function(error, doc) {
       // Log any errors
       if (error) {
-        console.log(error);
+        // console.log(error);
       }
       // Or send the doc to the browser as a json object
       else {
@@ -127,7 +134,7 @@ if(process.env.MONGODB_URI) {
         .exec(function(err, doc) {
           // Log any errors
           if (err) {
-            console.log(err);
+            // console.log(err);
           }
           else {
             // Or send the document to the browser
@@ -144,7 +151,7 @@ if(process.env.MONGODB_URI) {
         .exec(function(err, doc) {
           // Log any errors
           if (err) {
-            console.log(err);
+            // console.log(err);
           }
           else {
             // Or send the document to the browser
@@ -161,12 +168,12 @@ if(process.env.MONGODB_URI) {
       body: req.body.text,
       article: req.params.id
     });
-    console.log(req.body)
+    // console.log(req.body)
     // And save the new note the db
     newNote.save(function(error, note) {
       // Log any errors
       if (error) {
-        console.log(error);
+        // console.log(error);
       }
       // Otherwise
       else {
@@ -176,7 +183,7 @@ if(process.env.MONGODB_URI) {
         .exec(function(err) {
           // Log any errors
           if (err) {
-            console.log(err);
+            // console.log(err);
             res.send(err);
           }
           else {
@@ -194,7 +201,7 @@ if(process.env.MONGODB_URI) {
     Note.findOneAndRemove({ "_id": req.params.note_id }, function(err) {
       // Log any errors
       if (err) {
-        console.log(err);
+        // console.log(err);
         res.send(err);
       }
       else {
@@ -203,7 +210,7 @@ if(process.env.MONGODB_URI) {
           .exec(function(err) {
             // Log any errors
             if (err) {
-              console.log(err);
+              // console.log(err);
               res.send(err);
             }
             else {
